b156a99b4d2826af6a1906c1e287a5c51dd94199
elmerbool <35117496+elmerbool@users.noreply.github.com>
2020-05-21 23:11:24 +1000

    Added Terminal processing and updated Neuron deactivation.

--- src/main/Application/Neurons/TerminalCommandHandlers.cs
+++ src/main/Application/Neurons/TerminalCommandHandlers.cs
@@ -1,82 +1,55 @@
 ﻿using CQRSlite.Commands;
-using CQRSlite.Domain;
-using neurUL.Common.Domain.Model;
-// TODO: using neurUL.Common.Events;
 using ei8.Cortex.Diary.Nucleus.Application.Neurons.Commands;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using neurUL.Common.Domain.Model;
+using neurUL.Cortex.Client.In;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ei8.Cortex.Diary.Nucleus.Application.Neurons
 {
-    //public class TerminalCommandHandlers :
-    //    ICancellableCommandHandler<CreateTerminal>,
-    //    ICancellableCommandHandler<DeactivateTerminal>
-    //{
-        // TODO: private readonly IEventSourceFactory eventSourceFactory;
-        //private readonly IUserRepository userRepository;
-        //private readonly ILayerPermitRepository layerPermitRepository;
+    public class TerminalCommandHandlers :
+        ICancellableCommandHandler<CreateTerminal>,
+        ICancellableCommandHandler<DeactivateTerminal>
+    {
+        private readonly ITerminalClient terminalClient;
+        private readonly ISettingsService settingsService;
 
-        //public TerminalCommandHandlers(IEventSourceFactory eventSourceFactory, IUserRepository userRepository, ILayerPermitRepository layerPermitRepository)
-        //{
-        //    AssertionConcern.AssertArgumentNotNull(eventSourceFactory, nameof(eventSourceFactory));
-        //    // TODO: Add TDD test
-        //    AssertionConcern.AssertArgumentNotNull(userRepository, nameof(userRepository));
-        //    AssertionConcern.AssertArgumentNotNull(layerPermitRepository, nameof(layerPermitRepository));
+        public TerminalCommandHandlers(ITerminalClient terminalClient, ISettingsService settingsService)
+        {
+            AssertionConcern.AssertArgumentNotNull(terminalClient, nameof(terminalClient));
+            AssertionConcern.AssertArgumentNotNull(settingsService, nameof(settingsService));
 
-        //    this.eventSourceFactory = eventSourceFactory;
-        //    this.userRepository = userRepository;
-        //    this.layerPermitRepository = layerPermitRepository;
-        //}
+            this.terminalClient = terminalClient;
+            this.settingsService = settingsService;
+        }
 
-        //public async Task Handle(CreateTerminal message, CancellationToken token = default(CancellationToken))
-        //{
-        //    AssertionConcern.AssertArgumentNotNull(message, nameof(message));
+        public async Task Handle(CreateTerminal message, CancellationToken token = default(CancellationToken))
+        {
+            AssertionConcern.AssertArgumentNotNull(message, nameof(message));
 
-        //    var eventSource = this.eventSourceFactory.CreateEventSource(message.AvatarId);
-        //    await this.userRepository.Initialize(message.AvatarId);
-        //    await this.layerPermitRepository.Initialize(message.AvatarId);
+            await this.terminalClient.CreateTerminal(
+                this.settingsService.CortexInBaseUrl + "/",
+                message.Id.ToString(),
+                message.PresynapticNeuronId.ToString(),
+                message.PostsynapticNeuronId.ToString(),
+                message.Effect,
+                message.Strength,
+                message.AuthorId.ToString(),
+                token
+                );
+        }
 
-        //    Neuron presynaptic = await eventSource.Session.Get<Neuron>(message.PresynapticNeuronId, nameof(presynaptic), cancellationToken: token),
-        //        postsynaptic = await eventSource.Session.Get<Neuron>(message.PostsynapticNeuronId, nameof(postsynaptic), cancellationToken: token),
-        //        presynapticLayer = await eventSource.Session.GetOrDefaultIfGuidEmpty(
-        //            presynaptic.LayerId, 
-        //            nameof(presynapticLayer), 
-        //            Neuron.RootLayerNeuron, 
-        //            cancellationToken: token
-        //            );
+        public async Task Handle(DeactivateTerminal message, CancellationToken token = default(CancellationToken))
+        {
+            AssertionConcern.AssertArgumentNotNull(message, nameof(message));
 
-        //    // TODO: Add TDD test
-        //    var author = await NeuronCommandHandlers.GetValidatedAuthorUser(message.SubjectId, eventSource.Session, this.userRepository, this.layerPermitRepository, token);
-
-        //    var terminal = new Terminal(message.Id, presynaptic, presynapticLayer, postsynaptic, message.Effect, message.Strength, author);
-        //    await eventSource.Session.Add(terminal, token);
-        //    await eventSource.Session.Commit(token);
-        //}
-
-        //public async Task Handle(DeactivateTerminal message, CancellationToken token = default(CancellationToken))
-        //{
-        //    AssertionConcern.AssertArgumentNotNull(message, nameof(message));
-
-        //    var eventSource = this.eventSourceFactory.CreateEventSource(message.AvatarId);
-        //    await this.userRepository.Initialize(message.AvatarId);
-        //    await this.layerPermitRepository.Initialize(message.AvatarId);
-
-        //    // TODO: Add TDD test
-        //    var author = await NeuronCommandHandlers.GetValidatedAuthorUser(message.SubjectId, eventSource.Session, this.userRepository, this.layerPermitRepository, token);
-        //    Terminal terminal = await eventSource.Session.Get<Terminal>(message.Id, nameof(terminal), message.ExpectedVersion, token);
-        //    Neuron presynaptic = await eventSource.Session.Get<Neuron>(terminal.PresynapticNeuronId, nameof(presynaptic), cancellationToken: token);
-        //    Neuron presynapticLayer = await eventSource.Session.GetOrDefaultIfGuidEmpty(
-        //            presynaptic.LayerId,
-        //            nameof(presynapticLayer),
-        //            Neuron.RootLayerNeuron,
-        //            cancellationToken: token
-        //            );
-
-        //    terminal.Deactivate(presynaptic, presynapticLayer, author);
-        //    await eventSource.Session.Commit(token);
-        //}
-    //}
+            await this.terminalClient.DeactivateTerminal(
+                this.settingsService.CortexInBaseUrl + "/",
+                message.Id.ToString(),
+                message.ExpectedVersion,
+                message.AuthorId.ToString(),
+                token
+                );
+        }
+    }
 }
